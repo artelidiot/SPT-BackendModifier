f3d369086db6037325ad62ca85d12d2aac3c1652
artel <127449373+artelidiot@users.noreply.github.com>
2024-12-18 07:43:29 -0500

    Add some more controls and fix an oversight

--- src/mod.ts
+++ src/mod.ts
@@ -1,63 +1,92 @@
 import { DependencyContainer } from "tsyringe";
 
-import { IPostDBLoadMod } from "@spt/models/external/IPostDBLoadMod";
+import { ConfigServer } from "@spt/servers/ConfigServer";
+import { ConfigTypes } from "@spt/models/enums/ConfigTypes";
 import { DatabaseServer } from "@spt/servers/DatabaseServer";
+import { ICoreConfig } from "@spt/models/spt/config/ICoreConfig";
 import { IDatabaseTables } from "@spt/models/spt/server/IDatabaseTables";
 import { ILogger } from "@spt/models/spt/utils/ILogger";
+import { IPostDBLoadMod } from "@spt/models/external/IPostDBLoadMod";
 import { SkillTypes } from "@spt/models/enums/SkillTypes";
+import { IPreSptLoadMod } from "@spt/models/external/IPreSptLoadMod";
 
-class Mod implements IPostDBLoadMod {
+class Mod implements IPreSptLoadMod, IPostDBLoadMod {
 
-    public postDBLoad(container: DependencyContainer): void {
-        // get database from server
-        const databaseServer = container.resolve<DatabaseServer>("DatabaseServer")
-
-        // Get all the in-memory json found in /assets/database
-        const tables: IDatabaseTables = databaseServer.getTables()
-
-        // get the logger from the server container
+    public preSptLoad(container: DependencyContainer): void {
+        // Get the server container's logger
         const logger = container.resolve<ILogger>("WinstonLogger")
 
-        // Set the walking overweight limit to match the obese limit
-        // The 'x' value cannot be set to a value greater than the 'y' value, otherwise you will lose stamina while walking even while underweight (EFT bug/quirk?)
-        tables.globals.config.Stamina.WalkOverweightLimits.x = tables.globals.config.Stamina.WalkOverweightLimits.y
-        logger.debug("[database-modifier] set walkOverweightLimit.x to: " + tables.globals.config.Stamina.WalkOverweightLimits.x)
-        logger.info("[DatabaseModifier] Adjusted walking overweight limit")
+        // Get the configuration files from the server
+        const configServer = container.resolve<ConfigServer>("ConfigServer")
+        // Obtain the SPT_Data\Server\configs\core.json configuration file from the server
+        const coreConfig: ICoreConfig = configServer.getConfig<ICoreConfig>(ConfigTypes.CORE)
 
-        // Set the aim drain rate to match that of the range finder's
-        tables.globals.config.Stamina.AimDrainRate = tables.globals.config.Stamina.AimRangeFinderDrainRate
-        logger.debug("[database-modifier] set aimDrainRate to: " + tables.globals.config.Stamina.AimDrainRate)
-        logger.info("[DatabaseModifier] Adjusted aiming stamina drain rate")
+        // Disable the Commando chat bot
+        coreConfig.features.chatbotFeatures.commandoEnabled = false
+        logger.debug("[backend-modifier] set commandoEnabled to: " + coreConfig.features.chatbotFeatures.commandoEnabled)
+        coreConfig.features.chatbotFeatures.commandoFeatures.giveCommandEnabled = false
+        logger.debug("[backend-modifier] set commandoFeatures.giveCommandEnabled to: " + coreConfig.features.chatbotFeatures.commandoFeatures.giveCommandEnabled)
+        logger.info("[BackendModifier] Disabled the Commando chat bot")
+
+        // Disable the SPT chat bot
+        coreConfig.features.chatbotFeatures.sptFriendEnabled = false
+        logger.debug("[backend-modifier] set sptFriendEnabled to: " + coreConfig.features.chatbotFeatures.sptFriendEnabled)
+        coreConfig.features.chatbotFeatures.sptFriendGiftsEnabled = false
+        logger.debug("[backend-modifier] set sptFriendGiftsEnabled to: " + coreConfig.features.chatbotFeatures.sptFriendGiftsEnabled)
+        logger.info("[BackendModifier] Disabled the SPT chat bot")
+    }
+
+    public postDBLoad(container: DependencyContainer): void {
+        // Get the server container's logger
+        const logger = container.resolve<ILogger>("WinstonLogger")
+
+        // Get the database from the server
+        const databaseServer = container.resolve<DatabaseServer>("DatabaseServer")
+        // Get all the in-memory json found in /assets/database
+        const database: IDatabaseTables = databaseServer.getTables()
+
+        // Set the flea market to found-in-raid only
+        database.globals.config.RagFair.isOnlyFoundInRaidAllowed = true
+        logger.debug("[backend-modifier] set isOnlyFoundInRaidAllowed to: " + database.globals.config.RagFair.isOnlyFoundInRaidAllowed)
+        logger.info("[BackendModifier] Set flea market to found-in-raid only")
+
+        // Set the weapon stamina drain rate to match the range finder's stamina drain rate
+        database.globals.config.Stamina.AimDrainRate = database.globals.config.Stamina.AimRangeFinderDrainRate
+        logger.debug("[backend-modifier] set AimDrainRate to: " + database.globals.config.Stamina.AimDrainRate)
+        logger.info("[BackendModifier] Adjusted aiming stamina drain rate")
+
+        // Set the walking overweight limit to match the critical overweight limit
+        // The 'x' value cannot be set higher than the 'y' value, otherwise you will lose stamina while walking even while underweight (EFT bug/quirk?)
+        database.globals.config.Stamina.WalkOverweightLimits.x = database.globals.config.Stamina.WalkOverweightLimits.y
+        logger.debug("[backend-modifier] set WalkOverweightLimits.x to: " + database.globals.config.Stamina.WalkOverweightLimits.x)
+        logger.info("[BackendModifier] Adjusted walking overweight limit")
 
         // Iterate over all bot types
-        for (const botType in tables.bots.types) {
+        for (const botType in database.bots.types) {
             // Iterate over any skills present
-            for (const commonSkill in tables.bots.types[botType].skills.Common) {
+            for (const commonSkill in database.bots.types[botType].skills.Common) {
                 // Make sure the skill type is BotSound
                 if (commonSkill != SkillTypes.BOT_SOUND) continue
 
-                // Create a quick variable to clean up the code
-                const botSoundSkill = tables.bots.types[botType].skills.Common[commonSkill];
-
                 // Check if the minimum skill progress is elite level (5100)
-                if (botSoundSkill.min >= 5100) {
+                if (database.bots.types[botType].skills.Common[commonSkill].min >= 5100) {
                     // Set the minimum skill progress to 3000
-                    botSoundSkill.min = 3000
-                    logger.debug("[database-modifier] set minimum BotSound skill progress to: " + 3000 + " for botType: " + botType)
+                    database.bots.types[botType].skills.Common[commonSkill].min = 3000
+                    logger.debug("[backend-modifier] set minimum BotSound skill progress to: " + database.bots.types[botType].skills.Common[commonSkill].min + " for botType: " + botType)
                 }
 
                 // Check if the maximum skill progress is elite level (5100)
-                if (botSoundSkill.max >= 5100) {
+                if (database.bots.types[botType].skills.Common[commonSkill].max >= 5100) {
                     // Set the maximum skill progress to 3000
-                    botSoundSkill.max = 5100
-                    logger.debug("[database-modifier] set maximum BotSound skill progress to: " + 3000 + " for botType: " + botType)
+                    database.bots.types[botType].skills.Common[commonSkill].max = 3000
+                    logger.debug("[backend-modifier] set maximum BotSound skill progress to: " + database.bots.types[botType].skills.Common[commonSkill].max + " for botType: " + botType)
                 }
             }
         }
-        logger.info("[DatabaseModifier] Adjusted bot skills")
+        logger.info("[BackendModifier] Adjusted bot skills")
 
         // Iterate over every hideout area
-        tables.hideout.areas.forEach(area => {
+        database.hideout.areas.forEach(area => {
             // Iterate over the area's stages
             for (const stage in area.stages) {
                 // Just in case
@@ -68,10 +97,10 @@ class Mod implements IPostDBLoadMod {
 
                 // Divide the construction time by 7
                 area.stages[stage].constructionTime = area.stages[stage].constructionTime / 7
-                logger.debug("[database-modifier] set constructionTime to: " + area.stages[stage].constructionTime + " in stage: " + stage + " for area: " + area._id)
+                logger.debug("[backend-modifier] set constructionTime to: " + area.stages[stage].constructionTime + " in stage: " + stage + " for area: " + area._id)
             }
         })
-        logger.info("[DatabaseModifier] Adjusted hideout area construction times")
+        logger.info("[BackendModifier] Adjusted hideout area construction times")
     }
 }
 
